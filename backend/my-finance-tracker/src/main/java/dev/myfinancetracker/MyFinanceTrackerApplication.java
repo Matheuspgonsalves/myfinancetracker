3f1b5bbbde287df2fd81f3480f763e5cacdf58ac
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-11-23 22:58:23 -0300

    Criando as estruturas basicas principais para o Frontend e Backend

---
+++ backend/my-finance-tracker/src/main/java/dev/myfinancetracker/MyFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package dev.myfinancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFinanceTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/my-finance-tracker/src/test/java/dev/myfinancetracker/MyFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.myfinancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
