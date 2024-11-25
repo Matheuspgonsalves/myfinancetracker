23c73be79920df74e789a698bd62a2e37c23de4b
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-11-25 00:01:56 -0300

    Adicionando a entidade User para criar um usuário

---
+++ backend/my-finance-tracker/src/main/java/dev/myfinancetracker/model/UserModel.java
@@ -0,0 +1,49 @@
+package dev.myfinancetracker.model;
+
+public class UserModel {
+    private int id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String passwordHash;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
